d7cdb308e4654457cc485cebbb90d61e8eab18a3
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-09-05 13:33:19 -0500

    @State was implemented to listen the changes of properties and rebuild ContentView

--- Dicee SwiftUI/ContentView.swift
+++ Dicee SwiftUI/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // @State allows us to listen a change on one or more properties
+    // @State creates a new ContentView with the new values
+    // @State is only for SwiftUI
+    @State var leftDiceNumber: Int = 1
+    @State var rightDiceNumber: Int = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNumber: self.leftDiceNumber)
+                    DiceView(diceNumber: self.rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    // Change ramdomly the number of Dicee
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(leftDiceNumber: 1, rightDiceNumber: 1)
+            .previewDevice(PreviewDevice(rawValue: "iPhone XR"))
+    }
+}
+
+struct DiceView: View {
+    
+    let diceNumber: Int
+    
+    var body: some View {
+        Image("dice\(self.diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
     }
 }
